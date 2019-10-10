57169c28640cb37d743e48e2b7ef1b7860d6f576
Ivan Malopinsky <imsky@users.noreply.github.com>
2019-10-10 08:52:36 -0400

    add history env vars to bash profile

--- setup.sh
+++ setup.sh
@@ -20,6 +20,13 @@ if [[ $(uname) = "Darwin" ]]; then
     echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
     echo 'export EDITOR=nano' >> ~/.bash_profile
     echo 'export PATH="/usr/local/bin:/usr/local/opt/gnu-getopt/bin:$PATH"' >> ~/.bash_profile
+    echo 'shopt -s histappend' >> ~/.bash_profile
+    echo 'export HISTSIZE=9999' >> ~/.bash_profile
+    echo 'export HISTFILESIZE=9999' >> ~/.bash_profile
+    echo 'export HISTCONTROL=ignoreboth' >> ~/.bash_profile
+    echo 'export HISTIGNORE=ls:bg:fg:history' >> ~/.bash_profile
+    echo 'export HISTTIMEFORMAT="%F %T "' >> ~/.bash_profile
+    echo 'export PROMPT_COMMAND="history -a"' >> ~/.bash_profile
     /usr/local/opt/fzf/install
     defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
     cp -i .gitconfig .nanorc ~/
