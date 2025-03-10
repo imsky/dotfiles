3c54a2b4863ca149ab710b621be324f6da7cda1d
Ivan Malopinsky <imsky@users.noreply.github.com>
2025-03-10 18:15:50 -0400

    update macos setup

--- setup.sh
+++ setup.sh
@@ -11,13 +11,18 @@ if [[ $(uname) = "Darwin" ]]; then
 
   if [[ -z $(which brew) ]]; then
     # Install Homebrew
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.bash_profile
+    eval "$(/usr/local/bin/brew shellenv)"
+    # really can just do: git config --global --add safe.directory '*'
+    git config --global --add safe.directory /usr/local/Homebrew
   fi
 
   brew bundle install --verbose --file=macos/Brewfile
 
   if [[ -z $(grep 'setup complete' ~/.bash_profile | cat) ]]; then
     echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
+
     echo 'export EDITOR=nano' >> ~/.bash_profile
     echo 'export PATH="/usr/local/bin:/usr/local/opt/gnu-getopt/bin:$PATH"' >> ~/.bash_profile
     echo 'shopt -s histappend' >> ~/.bash_profile
@@ -27,11 +32,14 @@ if [[ $(uname) = "Darwin" ]]; then
     echo 'export HISTIGNORE=ls:bg:fg:history' >> ~/.bash_profile
     echo 'export HISTTIMEFORMAT="%F %T "' >> ~/.bash_profile
     echo 'export PROMPT_COMMAND="history -a"' >> ~/.bash_profile
-    echo 'export AWS_SDK_LOAD_CONFIG=1' >> ~/.bash_profile
-    /usr/local/opt/fzf/install
+    echo '[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"' >> ~/.bash_profile
+    # direnv hook MUST be last item to modify PROMPT_COMMAND
+    echo 'eval "$(direnv hook bash)"' >> ~/.bash_profile
+    eval "$(fzf --bash)"
 
     defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
     cp -i .gitconfig .nanorc ~/
-    echo '# setup complete\n\nRemember to customize .gitconfig' >> ~/.bash_profile
+
+    echo '# setup complete' >> ~/.bash_profile
   fi
 fi
